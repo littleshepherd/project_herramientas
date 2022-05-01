2fe5f8d887bd4bf1b1a25974e56ce82413b7548c
littleshepherd <beelthor@gmail.com>
2022-05-01 13:39:01 -0500

    cambio minimo

--- Controller/api/clientController.php
+++ Controller/api/clientController.php
@@ -4,7 +4,7 @@ include_once __DIR__.'/../../Model/db.php';
 class clientController {
     public function contact($data) {
         $db = new dataBase();
-        $query = "INSERT INTO contacts (name, email,phone, message) VALUES ($data->name, $data->email, $data->phone, $data->message)";
+        $query = "INSERT INTO clients (name, email,phone, message) VALUES ($data->name, $data->email, $data->phone, $data->message)";
         $db->execute($query);
     }
 }
